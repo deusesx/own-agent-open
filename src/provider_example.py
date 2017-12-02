7357ff5ef271dac6a783f5205e62352272bd8dd2
Egor <egorr.osokin@gmail.com>
2017-12-02 15:23:23 +0300

    Added reputation parser and getter now can get info by token address

--- src/provider_example.py
+++ src/provider_example.py
@@ -1,6 +1,13 @@
 from provider import TokenInfoProvider
 
-token_name = 'aragon'
+# good for parsing: aragon, golem, civic, qtum, eos, storj
+token_name = 'golem'
+
+# some tokens don't have a shortcut name at etherscan
+# in this cases it's better to specify its address manually and pass it to constructor
+# token_addr = '0xd697A61D5FB4e076125e0bE647f902b02bb3A0F1'
+
 token = TokenInfoProvider(token_name)
 print(token.info)
 print(token.links)
+print(token.reputation)
