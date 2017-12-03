b7e1e622b02f91b6adc69950c36504710f3e69e7
Артур Самигуллин <artur.samigullin@akbarsdigital.ru>
2017-12-03 18:19:48 +0300

    Widget 1

--- OWN_ICO_Widgets/OWN_ICO_Widgets.py
+++ OWN_ICO_Widgets/OWN_ICO_Widgets.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask import jsonify
 from flask import render_template
@@ -11,44 +10,22 @@ from OWN_ICO_Widgets.blockchain.contract_analysis import holders_chart_data
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
-@app.route('/token_profile/', methods=['GET'])
-def make_token_profile_widget():
-    name = request.args.get('token_short_name', None)
-    if name is not None:
-        return render_template("token_profile.html",
-                               token_short_name=name)
-    else:
-        return jsonify({'status': 400,
-                        'error_message': 'No token short name provided!'})
-
-
-@app.route('/ico_profile/', methods=['GET'])
-def make_ico_profile_widget():
-    name = request.args.get('token_name', None)
-    if name is not None:
-        return render_template("ico_profile.html",
-                               token_name=name)
-    else:
-        return jsonify({'status': 400,
-                        'error_message': 'No token short name provided!'})
 
 @app.route('/ico_info/', methods=['GET'])
 def make_ico_info_widget():
     name = request.args.get('token_name', None)
     price_usd, price_btc, total_volume, market_cap, change_percent = token_summary_data(name)
     short_name = symbol(name)
-    color = None
-    if change_percent>0:
+    color = "red"
+    if change_percent > 0:
         color = "green"
-    elif change_percent==0:
+    if change_percent == 0:
         color = "yellow"
-    else:
-        color = "red"
     if name is not None:
         return render_template("index.html",
                                token_name=name,
@@ -64,29 +41,70 @@ def make_ico_info_widget():
         return jsonify({'status': 400,
                         'error_message': 'Wrong token name provided!'})
 
-@app.route('/holders/', methods=['GET'])
-def make_holders_widget():
-    name = request.args.get('token_name', None)
-    labels, data = holders_chart_data(name)
 
-    if labels is not None:
-        return render_template('pie_chart.html', values=data, labels=labels)
-    else:
-        return jsonify({'status': 400,
-                        'error_message': 'Wrong token name provided!'})
 
-'''
-@app.route('/diagram/', methods=['GET'])
-def make_diagram_widget():
-    name = request.args.get('token_name', None)
-    labels, data = get_transaction_data_for_chart_by_name(name)
 
-    if labels is not None:
-        return render_template('diagram.html', values=data, labels=labels)
-    else:
-        return jsonify({'status': 400,
-                        'error_message': 'No token short name provided!'})
-'''
+
+
+
+
+# @app.route('/token_summary/', methods=['GET'])
+# def make_token_profile_widget():
+#     name = request.args.get('token_name', None)
+#     if name is not None:
+#         return render_template("index.html",
+#                                token_name=name)
+#     else:
+#         return jsonify({'status': 400,
+#                         'error_message': 'No token short name provided!'})
+
+# @app.route('/ico_profile/', methods=['GET'])
+# def make_ico_profile_widget():
+#     name = request.args.get('token_name', None)
+#     if name is not None:
+#         return render_template("ico_profile.html",
+#                                token_name=name)
+#     else:
+#         return jsonify({'status': 400,
+#                         'error_message': 'No token short name provided!'})
+#
+#
+#
+# @app.route('/holders/', methods=['GET'])
+# def make_holders_widget():
+#     name = request.args.get('token_name', None)
+#     labels, data = holders_chart_data(name)
+#
+#     if labels is not None:
+#         return render_template('pie_chart.html', values=data, labels=labels)
+#     else:
+#         return jsonify({'status': 400,
+#                         'error_message': 'Wrong token name provided!'})
+#
+# @app.route('/token_profile/', methods=['GET'])
+# def make_token_profile_widget():
+#     name = request.args.get('token_short_name', None)
+#     if name is not None:
+#         return render_template("token_profile.html",
+#                                token_name=name)
+#     else:
+#         return jsonify({'status': 400,
+#                         'error_message': 'No token short name provided!'})
+#
+#
+# @app.route('/token_profile/', methods=['GET'])
+# def make_token_profile_widget():
+#     name = request.args.get('token_short_name', None)
+#     if name is not None:
+#         return render_template("token_profile.html",
+#                                token_name=name)
+#     else:
+#         return jsonify({'status': 400,
+#                         'error_message': 'No token short name provided!'})
+
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='8080')
+    app.run(host='0.0.0.0', port='80')
